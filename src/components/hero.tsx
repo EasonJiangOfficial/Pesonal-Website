2cfda3f6628f13e3e6345d403e5d15fc4f66d118
Eason Jiang <EasonJiangabc@gmail.com>
2023-07-23 17:19:36 -0500

    typing animation

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,16 +1,29 @@
 import React from "react";
 import Image from "next/image";
+import TypeIt from "typeit-react";
 
 export default function Hero() {
   return (
     <>
       <div className="flex flex-row w-full">
         <div className="flex flex-col">
-          <h1>Hi Im Eason</h1>
-          <h2>Im a Full Stack Developer</h2>
+          <h1 className="text-8xl" text-lg>
+            Hi Im Eason
+          </h1>
+          <h1 className="text-5xl" text-lg>
+            Im a{" "}
+            <TypeIt
+              className="text-5xl"
+              element={"h1"}
+              options={{
+                strings: ["Full Stack Developer"],
+
+                loop: true,
+              }}
+            ></TypeIt>
+          </h1>
         </div>
-        <div className="flex flex-col">
-        </div>
+        <div className="flex flex-col"></div>
       </div>
     </>
   );
