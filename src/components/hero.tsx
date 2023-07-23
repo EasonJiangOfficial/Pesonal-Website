c22fd6d60e0eafdcd42d92e255596ccc1531688b
Eason Jiang <EasonJiangabc@gmail.com>
2023-07-23 16:57:57 -0500

    added tailwind

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,13 +4,14 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <>
-      <div className="black flex-row">
-        <h1>Hi Im Eason</h1>
-        <h2>Im a Full Stack Developer</h2>
-      </div>
-      <div className="">
-        <Image src="/images/Logo.jpeg" alt="hero" width={500} height={500} />
+      <div className="flex flex-row w-full">
+        <div className="flex flex-col">
+          <h1>Hi Im Eason</h1>
+          <h2>Im a Full Stack Developer</h2>
         </div>
+        <div className="flex flex-col">
+        </div>
+      </div>
     </>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.css'
+import "../../public/fonts/cubano.css"
 
 function MyApp({ Component, pageProps }:AppProps) {
   return <Component {...pageProps} />
