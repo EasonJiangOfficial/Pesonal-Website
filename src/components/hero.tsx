0020f8e571f804b7cca222341421768e7eec600c
Eason Jiang <EasonJiangabc@gmail.com>
2023-07-23 18:41:11 -0500

    added color

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import TypeIt from "typeit-react";
 
 export default function Hero() {
+  const [color, setColor] = useState("");
+
+  useEffect(() => {console.log(color)}, [color]);
+
+  const colorClasses: { [key: string]: string } = {
+    'custom-green': 'text-green-600',
+    'custom-yellow': 'text-yellow-600',
+    'custom-red': 'text-red-600',
+    'custom-blue': 'text-blue-600',
+    'custom-purple': 'text-purple-600',
+    'custom-pink': 'text-pink-600',
+  };
+  
+  
   return (
     <>
       <div className="flex flex-row w-full">
@@ -13,12 +27,50 @@ export default function Hero() {
           <h1 className="text-5xl" text-lg>
             Im a{" "}
             <TypeIt
-              className="text-5xl"
-              element={"h1"}
+              className={`text-5xl ${colorClasses[color]}`}
+              element={"span"}
               options={{
-                strings: ["Full Stack Developer"],
-
                 loop: true,
+              }}
+              getBeforeInit={(instance) => {
+                instance
+                  .exec(async() => {
+                    await setColor("custom-green");
+                  })
+                  .type("Full Stack Developer.")
+                  .pause(500)
+                  .delete()
+                  .exec(async() => {
+                    await setColor("custom-yellow");
+                  })
+                  .type("Entrepreneur.")
+                  .pause(500)
+                  .delete()
+                  .exec(async() => {
+                    await setColor("custom-red");
+                  })
+                  .type("Cook.")
+                  .pause(500)
+                  .delete()
+                  .exec(async() => {
+                    await setColor("custom-blue");
+                  })
+                  .type("Student.")
+                  .pause(500)
+                  .delete()
+                  .exec(async() => {
+                    await setColor("custom-purple");
+                  })
+                  .type("Musician.")
+                  .pause(500)
+                  .delete()
+                  .exec(async() => {
+                    await setColor("custom-pink");
+                  })
+                  .type("Video Editor.")
+                  .pause(500)
+                  .delete();
+                return instance;
               }}
             ></TypeIt>
           </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        "custom-blue":"#4E80EE",
+        "custom-green":"#5EC269",
+        "custom-red":"#DD524C",
+        "custom-yellow":"#E2B53E",
+        "custom-purple":"#9D59EF",
+        "custom-pink":"#DA5597",
+      }
     },
   },
   plugins: [],
