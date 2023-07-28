2be4f3884198b33823904e8a7061cf79be560a2b
Eason Jiang <EasonJiangabc@gmail.com>
2023-07-28 18:56:58 -0500

    added gradients

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,22 +1,17 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import TypeIt from "typeit-react";
+import Typer from "./typer";
 
 export default function Hero() {
-  const [color, setColor] = useState("");
-
-  useEffect(() => {console.log(color)}, [color]);
-
-  const colorClasses: { [key: string]: string } = {
-    'custom-green': 'text-green-600',
-    'custom-yellow': 'text-yellow-600',
-    'custom-red': 'text-red-600',
-    'custom-blue': 'text-blue-600',
-    'custom-purple': 'text-purple-600',
-    'custom-pink': 'text-pink-600',
+  const gradientClasses: { [key: string]: string } = {
+    "custom-green": "text-gradient-green",
+    "custom-orange": "text-gradient-orange",
+    "custom-red": "text-gradient-red",
+    "custom-blue": "text-gradient-blue",
+    "custom-purple": "text-gradient-purple",
+    "custom-pink": "text-gradient-pink",
   };
-  
-  
+
   return (
     <>
       <div className="flex flex-row w-full">
@@ -25,54 +20,7 @@ export default function Hero() {
             Hi Im Eason
           </h1>
           <h1 className="text-5xl" text-lg>
-            Im a{" "}
-            <TypeIt
-              className={`text-5xl ${colorClasses[color]}`}
-              element={"span"}
-              options={{
-                loop: true,
-              }}
-              getBeforeInit={(instance) => {
-                instance
-                  .exec(async() => {
-                    await setColor("custom-green");
-                  })
-                  .type("Full Stack Developer.")
-                  .pause(500)
-                  .delete()
-                  .exec(async() => {
-                    await setColor("custom-yellow");
-                  })
-                  .type("Entrepreneur.")
-                  .pause(500)
-                  .delete()
-                  .exec(async() => {
-                    await setColor("custom-red");
-                  })
-                  .type("Cook.")
-                  .pause(500)
-                  .delete()
-                  .exec(async() => {
-                    await setColor("custom-blue");
-                  })
-                  .type("Student.")
-                  .pause(500)
-                  .delete()
-                  .exec(async() => {
-                    await setColor("custom-purple");
-                  })
-                  .type("Musician.")
-                  .pause(500)
-                  .delete()
-                  .exec(async() => {
-                    await setColor("custom-pink");
-                  })
-                  .type("Video Editor.")
-                  .pause(500)
-                  .delete();
-                return instance;
-              }}
-            ></TypeIt>
+            Im a <Typer gradientClasses={gradientClasses} />
           </h1>
         </div>
         <div className="flex flex-col"></div>
