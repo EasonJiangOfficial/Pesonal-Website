21f328ebc61801ae48ef0557656715cec2bf8e91
Eason Jiang <EasonJiangabc@gmail.com>
2023-08-08 17:27:03 -0500

    added project tech icons

--- src/components/Projects/projectsList.tsx
+++ src/components/Projects/projectsList.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Link from "next/link";
-// import {TbBrandNextjs} from "react-icons/Tb"
-// import {FaReact,FaHtml5,FaCss3} from "react-icons/Fa"
-// import {SiWeb3Dotjs,SiJavascript,SiTypescript,SiTailwindcss} from "react-icons/Si"
+import { TbBrandNextjs } from "react-icons/Tb";
+import { FaReact, FaHtml5, FaCss3,FaInfinity } from "react-icons/Fa";
+import {
+  SiWeb3Dotjs,
+  SiJavascript,
+  SiTypescript,
+  SiTailwindcss,
+  SiFirebase
+} from "react-icons/Si";
 
 interface Project {
   description: string;
@@ -14,7 +20,24 @@ interface ProjectsListProps {
   projects: Project[];
 }
 
+interface TechLogos {
+  [key: string]: JSX.Element;
+}
+
 const ProjectsList: React.FC<ProjectsListProps> = ({ projects }) => {
+  const techLogos : TechLogos = {
+    "NextJs": <TbBrandNextjs className=""/>,
+    "React": <FaReact className=""/>,
+    "HTML": <FaHtml5 className=""/>,
+    "CSS": <FaCss3 className=""/>,
+    "Web3": <SiWeb3Dotjs className=""/>,
+    "JavaScript": <SiJavascript className=""/>,
+    "TypeScript": <SiTypescript className=""/>,
+    "Tailwind": <SiTailwindcss className=""/>,
+    "Firebase": <SiFirebase className=""/>,
+    "Motoko": <FaInfinity className=""/>,
+  };
+
   return (
     <div className="mt-28 gap-12 place-items-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
       {projects.map((project, index) => (
@@ -25,15 +48,18 @@ const ProjectsList: React.FC<ProjectsListProps> = ({ projects }) => {
           <div className="h-3/6"></div>
           <div className="flex flex-col gap-2">
             <Link href={project.github}>
-              <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl h-4/6 font-header">{project.name}</h1>
+              <h1 className="text-3xl sm:text-3xl md:text-4xl lg:text-5xl h-4/6 font-header">
+                {project.name}
+              </h1>
             </Link>
             <p className="text-2 font-body">
-              {project.description.slice(0, 400) + "..."}
+              {project.description.slice(0, 1000) + "..."}
             </p>
-            {/* {project.technologies.map((tech) => {
-              
-              return ();
-            })} */}
+            <div className="w-full flex flex-row gap-2">
+              {project.technologies.map((tech) => {
+                return techLogos[tech];
+              })}
+            </div>
           </div>
         </div>
       ))}
