4f3f875b0e72f178d862d9e1f6babf2d1c1eae0d
Eason Jiang <EasonJiangabc@gmail.com>
2023-08-20 22:12:21 -0500

    fixed mobile alignment

--- src/components/aboutMe.tsx
+++ src/components/aboutMe.tsx
@@ -3,15 +3,16 @@ import Image from "next/image";
 
 export default function AboutMe() {
   return (
-    <div className=" items-center justify-center w-full min-h-screen flex flex-col-reverse sm:flex-col-reverse md:flex-col-reverse lg:flex-row gap-10">
-      <div className="w-3/6 gap-2 flex flex-col">
+    <div className="items-center justify-center w-full min-h-screen flex flex-col-reverse lg:flex-row gap-10">
+      <div className="w-full sm:w-full md:w-3/6 lg:w-3/6 gap-2 flex flex-col">
         <h1
-          className="font-header text-5xl sm:text-5xl md:text-7xl lg:text-8xl text-white"
+          className="font-header text-6xl md:text-7xl lg:text-8xl text-center lg:text-left text-white"
           data-aos="fade-up"
         >
           About Me
         </h1>
-        <p className="text-2 font-body text-white" data-aos="fade-right">
+        <p className="text-base font-body text-white text-center lg:text-left lg:text-lg" data-aos="fade-right">
+
           Im a 16-year-old high school junior from Missouri. In my free time, I
           like to learn new skills like coding, video editing, playing musical
           instruments, UI design, cooking, etc.; read books about personal
