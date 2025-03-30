6580f98edf0f8a8c2784d140368e0800c3f2d2d3
Eason Jiang <EasonJiangabc@gmail.com>
2025-03-30 16:55:58 -0500

    padding

--- src/components/blog/blogList.jsx
+++ src/components/blog/blogList.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function BlogList({ blogPosts }) {
   return (
-    <div className="mt-28 gap-12 place-items-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+    <div className="mt-28 gap-12 place-items-center grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
       {blogPosts.map((blogPost, index) => (
         <Link
         key={index}
@@ -15,7 +15,7 @@ export default function BlogList({ blogPosts }) {
         >
         <div
           key={index}
-          className="hover:from-gradient-end-red hover:to-red-800 hover:-translate-y-4 hover:shadow-2xl hover:ease-in duration-200 rounded-[30px] bg-gradient-to-b from-gradient-start-red to-gradient-end-red p-4 w-full h-[30em] sm:h-[30rem] md:h-[32rem] lg:h-[34rem] flex flex-col justify-center items-center text-white"
+          className="hover:from-gradient-end-red hover:to-red-800 hover:-translate-y-4 hover:shadow-2xl hover:ease-in duration-200 rounded-[30px] bg-gradient-to-b from-gradient-start-red to-gradient-end-red p-10 w-full h-[30em] sm:h-[30rem] md:h-[32rem] lg:h-[34rem] flex flex-col justify-center items-center text-white"
           data-aos="fade-right"
           data-aos-delay={`${index * 100}`}
         >
@@ -35,7 +35,7 @@ export default function BlogList({ blogPosts }) {
             </div>
 
             <p className="text-2 font-body break-words">
-              {blogPost.content.slice(0, 100) + "..."}
+              {blogPost.content.slice(0, 700) + "..."}
             </p>
           </div>
         </div>
